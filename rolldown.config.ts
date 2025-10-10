53f37af461892a69dfaaf579d4d55ae0bef3aa20
James Aprosail <aprosail@outlook.com>
2025-10-10 22:49:27 +0800

    fix: external dependencies

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,10 +1,11 @@
 import { builtinModules } from "node:module"
 import { defineConfig } from "rolldown"
 import { dts } from "rolldown-plugin-dts"
+import { dependencies } from "./package.json"
 
 export default defineConfig({
   plugins: [dts({ sourcemap: true })],
-  external: [/^node:/g, ...builtinModules],
+  external: [/^node:/g, ...Object.keys(dependencies), ...builtinModules],
   input: "src/index.ts",
   output: { dir: "out", format: "esm", minify: true, sourcemap: true },
 })
