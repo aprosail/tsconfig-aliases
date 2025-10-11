f5cb0c5f28ff09bd69ed69a1d68ad84104fdb073
James Aprosail <aprosail@outlook.com>
2025-10-11 17:31:49 +0800

    chore: empty outdir before build

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,11 +1,17 @@
+import { readdirSync, rmSync } from "node:fs"
 import { builtinModules } from "node:module"
+import { join } from "node:path"
 import { defineConfig } from "rolldown"
 import { dts } from "rolldown-plugin-dts"
 import { dependencies } from "./package.json"
 
+const root = import.meta.dirname
+const out = join(root, "out")
+
+for (const n of readdirSync(out)) rmSync(join(out, n), { recursive: true })
 export default defineConfig({
   plugins: [dts({ sourcemap: true })],
   external: [/^node:/g, ...Object.keys(dependencies), ...builtinModules],
-  input: "src/index.ts",
-  output: { dir: "out", format: "esm", minify: true, sourcemap: true },
+  input: ["index"].map((n) => join(root, `src/${n}.ts`)),
+  output: { dir: out, format: "esm", minify: true, sourcemap: true },
 })
